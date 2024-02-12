1ff884196f3cae37242ab0a18ed647124b7022a9
omar123lecor <154367566+omar123lecor@users.noreply.github.com>
2024-02-12 22:35:22 +0100

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,42 @@
+import random
+import os
+liste = ["Rock", "Paper", "Scissors"]
+
+while True :
+    os.system("cls")
+    user_input = input("Rock , Paper , Scissors ? :")
+    computer = random.choice(liste)
+    if user_input in liste:
+        if computer == "Rock" and user_input == "Paper":
+            print(f"Computer chose {computer}")
+            print("Congratulations!, you win!")
+        elif computer == "Rock" and user_input == "Scissors":
+            print(f"Computer chose {computer}")
+            print("Sorry , You lose")
+        elif computer == "Paper" and user_input == "Rock":
+            print(f"Computer chose {computer}")
+            print("Sorry , You lose")
+        elif computer == "Paper" and user_input == "Scissors":
+            print(f"Computer chose {computer}")
+            print("Congratulations!, you win!")
+        elif computer == "Scissors" and user_input == "Rock":
+            print(f"Computer chose {computer}")
+            print("Congratulations!, you win!")
+        elif computer == "Scissors" and user_input == "Paper":
+            print(f"Computer chose {computer}")
+            print("Sorry , You lose")
+        else:
+            print(f"Computer chose {computer}")
+            print("Equality")
+        x = input("Do you want to contunie , Yes or No ?")
+        while x not in ["Yes","No"]:
+            os.system("cls")
+            print("invalide option please try again")
+            x = input("Do you want to contunie , Yes or No ?")
+        if x in ["Yes","No"]:
+            if x == "Yes":
+                continue
+            else:
+                break
+    else:
+        print("Invalid option")
